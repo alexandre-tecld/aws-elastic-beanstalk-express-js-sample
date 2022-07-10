7b37ee4b7275c4ee3a7a50cf4e7f11ea51f7e010
alexandre-tecld <teccld.contato@gmail.com>
2022-07-09 22:47:34 -0300

    att no arquivo app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ola Mundo para Aplicação!'));
+app.get('/', (req, res) => res.send('Ola Mundo para Aplicação do devops!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
