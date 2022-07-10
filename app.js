58c14ea2d272b9695e40ff63871bbd34d95d787d
alexandre-tecld <teccld.contato@gmail.com>
2022-07-10 00:49:05 -0300

    atualizando conteudo da pagina e testando pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ola Mundo para Aplicação do devops!'));
+app.get('/', (req, res) => res.send('Olá Mundo, DevOps é Vida! Sou Junior, preciso de uma oportunidade.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
